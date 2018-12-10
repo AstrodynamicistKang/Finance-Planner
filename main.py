a540a7d55dfec5cc3a679669fa9bf1c29e9f4594
Nicholas Kang <38155546+AstrodynamicistKang@users.noreply.github.com>
2018-12-10 10:07:17 +0800

    Added separate increment ratio for expenses.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import locale
 print("Finance Planner\n")
 
 while True:
@@ -7,7 +6,7 @@ while True:
 
     print()
 
-    z = eval(input("Enter maximum monthly expenses.\n"))
+    z = eval(input("Enter expenses increment ratio (in % percent) and maximum monthly expenses.\n"))
 
     print()
 
@@ -27,19 +26,19 @@ while True:
     new = inputs[0]
     cr = (inputs[1])/100 + 1
     exp = (inputs[2])/12
+    cre = z[0]/100 + 1
     cum = 0
 
     print("Age     Annual Salary   Monthly Salary   Monthly Expenses    Monthly Savings      Annual Savings     Cumulative Annual Savings")
 
     for n in range(0,y[1]+1-y[0]):
       new = new * cr
-      if exp < z:
-        exp = exp * cr
+      if exp < z[1]:
+        exp = exp * cre
       else:
-        exp = z 
-      cum = (new)-(exp*cr*12) + cum
-      print("{:3} {:15,.2f} {:15,.2f} {:17,.2f} {:18,.2f} {:20,.2f} {:25,.2f}".format(n+y[0], new, (new/12), (exp*cr) , ((new/12) - (exp*cr)), ((new)-(exp*cr*12)), cum))
+        exp = z[1] 
+      cum = (new)-(exp*cre*12) + cum
+      print("{:3} {:15,.2f} {:15,.2f} {:17,.2f} {:18,.2f} {:20,.2f} {:25,.2f}".format(n+y[0], new, (new/12), (exp*cre) , ((new/12) - (exp*cre)), ((new)-(exp*cre*12)), cum))
 
     print()
     print(126*"-" + 2*"\n")
-
